e1b6ae9320c131d79843529fa777064b9ef89a7b
C4FE1 <69542644+C4FE1@users.noreply.github.com>
2022-05-07 14:34:47 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ sudo su
 pacman -S nitrogen dunst network-manager-applet alacritty rofi bspwm sxhkd polybar compton git
 exit
 git clone https://github.com/C4FE1/Enjoy.DotFiles.git
-cp $HOME/Dot.Files/.config $HOME
+cp -r $HOME/Dot.Files/.config $HOME
 nitrogen $HOME/Dot.Files/Wallpaper/Enjoy.jpg
 rm -r $HOME/Dot.Files/.config 
 echo Obrigado por ficar de olho nele Aproveite :^)
