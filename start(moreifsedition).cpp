07ab0169af70072c92594ca44b68604b92f11318
C4FE1 <69542644+C4FE1@users.noreply.github.com>
2023-01-06 19:22:06 -0300

    Add files via upload

---
+++ start(moreifsedition).cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+using namespace std;
+int main() {
+    string interface;
+    cout << "Qual interface você quer acessar?\nOpções: Gnome, Bspwm, Hyprland ou Hypr. Por favor escreva tudo em letras minusculas\n";
+    cin >> interface;
+    if (interface == "gnome") {
+        system("exec gnome");
+    } else if (interface == "bspwm") {
+        system("exec bspwm");
+    } else if (interface == "hyprland") {
+        system("exec hyprland");
+    } else if (interface == "hypr") {
+	system("exec hypr");
+    }else {
+        cout << "Interface não reconhecida." << endl;
+    }
+    return 0;
+}
